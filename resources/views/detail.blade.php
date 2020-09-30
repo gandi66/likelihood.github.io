0b96f117934d3789be07863090aa6b0eddbca783
gandi66 <gandimah0123@gmail.com>
2020-09-30 15:10:01 +0700

    first commit

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Covid-19</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+	  <div class="bg-top navbar-light">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-center align-items-stretch">
+    			<div class="col-md-4 d-flex align-items-center py-4">
+    				<a class="navbar-brand" href="index.html">Likelihood Covid-19</a>
+    			</div>
+	    		<div class="col-lg-8 d-block">
+		    		<div class="row d-flex">
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+					    	<div class="text">
+					    		<span>Email</span>
+						    	<span>rumahsktcilacap@email.com</span>
+						    </div>
+					    </div>
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <div class="text">
+						    	<span>Call</span>
+						    	<span>Call Us: + 0282 533 010</span>
+						    </div>
+					    </div>
+					    <div class="col-md topper d-flex align-items-center justify-content-end">
+					    	
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </div>
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center px-4">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	      <form action="#" class="searchform order-lg-last">
+          <div class="form-group d-flex">
+            <input type="text" class="form-control pl-3" placeholder="Search">
+            <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
+          </div>
+        </form>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav mr-auto">
+	        	<li class="nav-item"><a href="home" class="nav-link pl-0">Home</a></li>
+	        	<li class="nav-item active"><a href="detail" class="nav-link">detail</a></li>
+	        	<li class="nav-item"><a href="login" class="nav-link">Login</a></li>
+	        
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/beg_1.jpg');">
+      
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Detail</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html"></a></span> <span></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+		<section class="ftco-section">
+			<div class="container">
+				<div class="row d-flex">
+					<div class="col-md-5 order-md-last wrap-about align-items-stretch">
+						<div class="wrap-about-border">
+							<div class="img" style="background-image: url(images/obout1.jpg); border"></div>
+							<div class="text">
+							<ul>
+         <li class="text-left text-black">Jenis Faskes:RSUD Cilacap</li>
+         <li class="text-left text-black">No Telepon: (0282) 533 010</li>
+         <li class="text-left text-black">Alamat RS:Jl.Gatot Subroto No.28.Tambaksari,Sidanegara,Kec.Cilacap Tengah,Kabupaten Cilacap,Jawa Tengah.</li>
+         <li class="text-left text-black">Jenis Tes:t:PCR/SWAB</li>     
+              </ul>
+
+								<p><a href="#" class="btn btn-primary py-3 px-4">Selanjutnya...</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-7 wrap-about pr-md-4 ftco-animate">
+          	<h2 class="mb-4">Selamat Datang Di RSUD Cilacap</h2>
+						<p>Rumah Sakit merupakan Lembaga Pemerintah yang menjalankan fungsi kesehatan yakni memberikan sarana dasar, upaya kesehatan rujukan dan upayakesehatan penunjang, dimana dalam penyelenggaraan harus memperhatikan fungsi sosial.</p>
+						<p>Sebagai fungsi sosial di bidang kesehatan, Rumah Sakit Umum Daerah Cilacap selanjutnya disingkat RSUD Cilacap merupakan pelayanan publik yang senantiasa melakukan pemasaran atas peran, fungsi dan manajemen Rumah Sakit. Oleh karenaRSUD Cilacap terus berbenah diri untuk mengembangkan kualitas ManajemenRumah Sakit, melaksanakan tugas dan fungsi rumah sakit secara profesional.</p>
+						<p>Rumah sakit RSUD ini juga sudah menyediakan tempat rapid test/swab,dan tempat penyakit lain-lain nya.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="ftco-intro ftco-no-pb img" style="background-image: url(images/cov.jpg);">
+    	<div class="container">
+    		<div class="row justify-content-center mb-5">
+          <div class="col-md-10 text-center heading-section heading-section-white ftco-animate">
+            <h2 class="mb-0">Update Pasien Covid-19</h2>
+          </div>
+        </div>	
+    	</div>
+    </section>
+
+		<section class="ftco-counter" id="section-counter">
+    	<div class="container">
+    		<div class="row d-md-flex align-items-center justify-content-center">
+    			<div class="wrapper">
+    				<div class="row d-md-flex align-items-center">
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="607">0</strong>
+		                <span>Pasien Positif</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="908">0</strong>
+		                <span>Pasien Sembuh</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="335">0</strong>
+		                <span>Pasien Meninggal</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="90">0</strong>
+		                <span>ODP</span>
+		              </div>
+		            </div>
+		          </div>
+	          </div>
+          </div>
+        </div>
+    	</div>
+    </section>
+		
+		<section class="ftco-section testimony-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-8 text-center heading-section ftco-animate">
+            <h2 class="mb-4">Temukan Dokter Spesialis</h2>
+            <p>Cari dan temukan dokter yang menurut anda tepat untuk kebutuhan Anda,Dan menerima rapid test/swab</p>
+          </div>
+        </div>
+        <div class="item">
+                <div class="testimony-wrap d-flex">
+                  <div class="user-img" style="background-image: url(images/poto.jpg)">
+                  </div>
+                  <div class="text pl-4">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                    <p>dr. Dewi Aryanti adalah seorang Dokter Umum yang bekerja di Rumah Sakit yang berada di daerah Cilacap Jawa Tengah.</p>
+                    <p class="name">Dr.Dewi Aryanti</p>
+                    <span class="position">Dokter Umum</span>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="testimony-wrap d-flex">
+                  <div class="user-img" style="background-image: url(images/poto_2.jpeg)">
+                  </div>
+                  <div class="text pl-4">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                    <p>dr. Haris Aripudin Lubis adalah seorang Dokter Umum yang berpraktik di Rumah Sakit Pertamina yang berada di daerah Cilacap.</p>
+                    <p class="name">Dr.Haris Aripudin Lubis</p>
+                    <span class="position">Dokter Umum</span>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="testimony-wrap d-flex">
+                  <div class="user-img" style="background-image: url(images/poto_3.jpeg)">
+                  </div>
+                  <div class="text pl-4">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                    <p>dr. Yessica Putri H. adalah seorang Dokter Umum yang berpraktik di Rumah Sakit RSUD yang berada di daerah Cilacap.</p>
+                    <p class="name">Dr.Yessica Putri H.</p>
+                    <span class="position">Dokter Umum</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+		
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2">Hubungi Kami Di?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	               
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+0282 533 010</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">rumahsktCilacap@email.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5 ml-md-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Detail</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Login</a></li>
+              
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2">Subscribe Us!</h2>
+              <form action="#" class="subscribe-form">
+                <div class="form-group">
+                  <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
+                  <input type="submit" value="Subscribe" class="form-control submit px-3">
+                </div>
+              </form>
+            </div>
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2 mb-0">Follow Kami Di</h2>
+            	<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Likelihood &copy;<script>document.write(new Date().getFullYear());</script> <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Gandi Wijaya</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
